9a1d95583320895d586eb804abf148f49b7536a2
Cho2unseo <escces00@gmail.com>
2025-07-21 17:22:43 +0900

    [✨ FEAT] 영수증 목록/상세 조회 API

---
+++ src/test/java/org/refit/spring/receipt/service/ReceiptServiceTest.java
@@ -0,0 +1,16 @@
+package org.refit.spring.receipt.service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReceiptServiceTest {
+
+    @DisplayName("구매 영수증 생성 API 테스트")
+    @Test
+    void create() {
+
+
+    }
+}
