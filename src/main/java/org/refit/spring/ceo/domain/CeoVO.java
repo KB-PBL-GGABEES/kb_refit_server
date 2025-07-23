0e19105845648d5c892f0078a02f083313f69acc
CHOKH <ghks027@gmail.com>
2025-07-23 10:37:04 +0900

    [🛠️FIX] 결제일시 타입 변경

--- src/main/java/org/refit/spring/ceo/domain/CeoVO.java
+++ src/main/java/org/refit/spring/ceo/domain/CeoVO.java
@@ -1,9 +1,12 @@
 package org.refit.spring.ceo.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
 
 @Data
 @Builder
@@ -13,8 +16,7 @@ public class CeoVO {
     private Long receiptId;
     private String companyName; // 상호명
     private Long totalPrice;    // 결제금액
-    private String receiptDate; // 결제일
-    private String receiptTime; // 결제시간
+    private LocalDateTime receiptDateTime; // 결제 일시
     private String processState; // 영수처리 여부
 
 //    private String storeImage;  // 상호이미지
