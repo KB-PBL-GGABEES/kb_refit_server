f18ca2cb62ec1781983446a2318870ad618f7521
yeonhookim <rladusgn68@gmail.com>
2025-07-23 11:31:02 +0900

    [🐛FIX]: 의료비 관련 자료형 수정

--- src/main/java/org/refit/spring/mapper/HospitalMapper.java
+++ src/main/java/org/refit/spring/mapper/HospitalMapper.java
@@ -5,6 +5,7 @@ import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 import org.refit.spring.hospital.dto.HospitalExpenseDetailResponseDto;
 import org.refit.spring.hospital.dto.HospitalExpenseResponseDto;
+import org.refit.spring.hospital.dto.HospitalRecentResponseDto;
 import org.refit.spring.hospital.dto.InsuranceSubscribedResponseDto;
 
 import java.util.List;
@@ -15,7 +16,7 @@ public interface HospitalMapper {
     // 의료비 납입내역 조회
     @Select("SELECT r.created_at AS createdAt, " +
             "c.company_name AS storeName, " +
-            "hp.process_state AS processState, " +
+            "hp.process_state AS processState, "  +
             "r.total_price AS totalPrice " +
             "FROM hospital_process hp " +
             "JOIN receipt r ON hp.receipt_id = r.receipt_id " +
@@ -38,11 +39,20 @@ public interface HospitalMapper {
          HospitalExpenseDetailResponseDto findByHospitalExpenseDetailId(@Param("receiptId") Long receiptId);
 
     // 최근 병원비 조회
+    @Select("SELECT COALESCE(SUM(r.total_price), 0) AS recentTotalPrice, " +
+            "COUNT(CASE WHEN r.transaction_type = '보험청구' THEN 1 END) AS insuranceBillable " +
+            "FROM receipt r " +
+            "JOIN company c ON r.company_id = c.company_id " +
+            "JOIN categories cat ON c.category_id = cat.category_id " +
+            "WHERE cat.category_name = '병원' " +
+            "AND r.created_at >= DATE_SUB(NOW(), INTERVAL 3 YEAR) " +
+            "AND r.user_id = #{userId}")
+    HospitalRecentResponseDto findByHospitalRecentId(@Param("userId") Long userId);
 
     // 가입된 보험 조회
     @Select("SELECT insurance_id AS insuranceId, " +
             "insurance_name AS insuranceName, " +
-            "DATE_FORMAT(joined_date, '%Y.%m.%d') AS joinedDate " +
+            "joined_date AS joinedDate " +
             "FROM insurance " +
             "WHERE user_id = #{userId}")
     List<InsuranceSubscribedResponseDto> findByInsuranceSubscribeId(@Param("userId") Long userId);
