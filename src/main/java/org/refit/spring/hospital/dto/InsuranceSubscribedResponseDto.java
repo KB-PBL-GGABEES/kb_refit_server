632bc46cdd888e1ed2b88a01010a54ba1c01029b
yeonhookim <rladusgn68@gmail.com>
2025-07-23 11:31:02 +0900

    [🐛FIX]: 의료비 관련 자료형 수정

--- src/main/java/org/refit/spring/hospital/dto/InsuranceSubscribedResponseDto.java
+++ src/main/java/org/refit/spring/hospital/dto/InsuranceSubscribedResponseDto.java
@@ -3,12 +3,12 @@ package org.refit.spring.hospital.dto;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.List;
+import java.util.Date;
 
 @Getter
 @Setter
 public class InsuranceSubscribedResponseDto {
-        private Long insuranceId;
-        private String insuranceName;
-        private String joinedDate;
-    }
+    private Long insuranceId;
+    private String insuranceName;
+    private Date joinedDate;
+}
