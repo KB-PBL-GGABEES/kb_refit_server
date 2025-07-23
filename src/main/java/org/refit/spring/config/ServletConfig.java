606ebcd93d178cb0549d5a1e95e949512e90ecaa
Cho2unseo <escces00@gmail.com>
2025-07-23 11:44:30 +0900

    [🐛 FIX] 영수증 정보 버그 수정

--- src/main/java/org/refit/spring/config/ServletConfig.java
+++ src/main/java/org/refit/spring/config/ServletConfig.java
@@ -15,7 +15,7 @@ import java.util.List;
         "org.refit.spring",
         "org.refit.spring.test",
         "org.refit.spring.auth.controller",
-        "org.refit.spring.ceo.controller"
+        "org.refit.spring.ceo.controller",
         "org.refit.spring.receipt.controller",
         "org.refit.spring.security"
 })
