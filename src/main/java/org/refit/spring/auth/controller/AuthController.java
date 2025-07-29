170121b0de991b0a2d254e1c0f8fed239ae8e2e9
suminiee <chansu0114@naver.com>
2025-07-29 17:30:22 +0900

    [♻️ REFACTOR] AuthController Swagger annoation 추가

--- src/main/java/org/refit/spring/auth/controller/AuthController.java
+++ src/main/java/org/refit/spring/auth/controller/AuthController.java
@@ -1,6 +1,8 @@
 package org.refit.spring.auth.controller;
 
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.refit.spring.auth.dto.LoginRequestDto;
 import org.refit.spring.auth.dto.UserResponseDto;
@@ -16,6 +18,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Collections;
 import java.util.Map;
 
+@Api(tags = "권한 관련 API", description = "로그인, refresh token, mydata 관련 API입니다..")
 @RestController
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
@@ -30,6 +33,7 @@ public class AuthController {
         return new BCryptPasswordEncoder().encode("050505");
     }
 
+    @ApiOperation(value = "현재 로그인한 유저 정보 조회 API", notes = "현재 로그인한 유저의 정보를 조회할 수 있습니다.")
     @GetMapping("/me")
     public ResponseEntity<?> getMyData(@RequestHeader("Authorization") String authHeader) {
         String token = authHeader.replace("Bearer ", "");
@@ -50,6 +54,7 @@ public class AuthController {
     }
 
 
+    @ApiOperation(value = "새 access token 발급", notes = "refresh token을 이용하여 새로운 access token을 발급하는 API입니다.")
     @PostMapping("/refresh")
     public ResponseEntity<?> refresh(@RequestHeader("Authorization") String refreshToken) {
         if (!jwtTokenProvider.validateRefreshToken(refreshToken)) {
