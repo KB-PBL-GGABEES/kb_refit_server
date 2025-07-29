1cd4c03742909be78920a331e2c2da7505f769b3
yeonhookim <rladusgn68@gmail.com>
2025-07-30 01:34:52 +0900

    [✨FEAT] 영수 처리 API

---
+++ src/main/java/org/refit/spring/receiptProcess/dto/ReceiptProcessRequestDto.java
@@ -0,0 +1,15 @@
+package org.refit.spring.receiptProcess.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+// 영수 처리 요청
+@Getter
+@Setter
+public class ReceiptProcessRequestDto {
+    private Long companyId;
+    private Long receiptId;
+    private String progressType;
+    private String progressDetail;
+    private String voucher;
+}
---
+++ src/main/java/org/refit/spring/receiptProcess/dto/ReceiptSelectDto.java
@@ -0,0 +1,20 @@
+package org.refit.spring.receiptProcess.dto;
+
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+// 사업장 선택 조회
+@Getter
+@Setter
+public class ReceiptSelectDto {
+    private String companyName;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
+    private Date startDate;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
+    private Date endDate;
+}
+
