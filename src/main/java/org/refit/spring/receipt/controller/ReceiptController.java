7d5a362f49d072c006b948a2fb72b7c269d2474c
Cho2unseo <escces00@gmail.com>
2025-07-30 16:20:18 +0900

    [🚀	 CHORE] Swagger 및 응답 형식 변경

--- src/main/java/org/refit/spring/receipt/controller/ReceiptController.java
+++ src/main/java/org/refit/spring/receipt/controller/ReceiptController.java
@@ -2,6 +2,8 @@ package org.refit.spring.receipt.controller;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.refit.spring.auth.annotation.UserId;
 import org.refit.spring.auth.service.UserService;
@@ -17,6 +19,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import springfox.documentation.annotations.ApiIgnore;
 
+import java.net.URI;
 import java.sql.SQLException;
 
 
@@ -33,6 +36,10 @@ public class ReceiptController {
     private static final Long CARBON_POINT = 100L;
 
     @ApiOperation(value = "영수증 등록", notes = "결제 시 영수증이 생성됩니다.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 400, message = "잘못된 요청"),
+            @ApiResponse(code = 500, message = "서버 내부 오류")
+    })
     @PostMapping("/create")
     public ResponseEntity<?> create(@ApiIgnore @UserId Long userId, @RequestBody ReceiptRequestDto receiptRequestDto) throws SQLException {
         Receipt receipt = receiptService.create(receiptRequestDto, userId);
@@ -40,20 +47,30 @@ public class ReceiptController {
         userService.updatePoint(userId, reward.getCarbonPoint(), reward.getReward());
         ReceiptResponseDto dto = ReceiptResponseDto.from(receipt, userId, reward.getCarbonPoint(), reward.getReward());
         receiptService.checkAndInsertBadge(userId, receipt.getReceiptId());
-        return ResponseEntity.ok(dto);
+        URI location = URI.create("/receipt/" + receipt.getReceiptId());
+        return ResponseEntity.created(location).body(dto);
     }
 
     @ApiOperation(value = "환불 영수증 등록", notes = "결제 시 생성된 영수증 아이디를 이용해 환불 영수증을 생성합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 400, message = "잘못된 요청"),
+            @ApiResponse(code = 500, message = "서버 내부 오류")
+    })
     @PostMapping("/refund")
     public ResponseEntity<?> refund(@ApiIgnore @UserId Long userId, @RequestParam("receiptId") Long receiptId) {
         Receipt receipt = receiptService.refund(userId, receiptId);
         Reward reward = rewardService.create(-CARBON_POINT, receipt.getTotalPrice(), userId);
         userService.updatePoint(userId, reward.getCarbonPoint(), reward.getReward());
         ReceiptResponseDto dto = ReceiptResponseDto.from(receipt, userId, reward.getCarbonPoint(), reward.getReward());
-        return ResponseEntity.ok(dto);
+        URI location = URI.create("/receipt/" + receipt.getReceiptId());
+        return ResponseEntity.created(location).body(dto);
     }
 
     @ApiOperation(value = "영수증 목록 조회", notes = "전체 영수증을 조회합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 400, message = "잘못된 요청"),
+            @ApiResponse(code = 500, message = "서버 내부 오류")
+    })
     @GetMapping("/list")
     public ResponseEntity<?> getList(
             @ApiIgnore @UserId Long userId,
@@ -64,6 +81,10 @@ public class ReceiptController {
 
 
     @ApiOperation(value = "영수증 상세 조회", notes = "영수증 아이디를 활용해 영수증에 기록된 모든 정보를 확인합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 400, message = "잘못된 요청"),
+            @ApiResponse(code = 500, message = "서버 내부 오류")
+    })
     @GetMapping("/get")
     public ResponseEntity<?> get(
             @ApiIgnore @UserId Long userId,
@@ -74,18 +95,30 @@ public class ReceiptController {
     }
 
     @ApiOperation(value = "한 달 비용 조회", notes = "이번 달과 지난 달의 사용 내역 합계를 확인합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 400, message = "잘못된 요청"),
+            @ApiResponse(code = 500, message = "서버 내부 오류")
+    })
     @GetMapping("/monthlyReport")
     public ResponseEntity<?> getTotal(@ApiIgnore @UserId Long userId) {
         return ResponseEntity.ok(receiptService.getTotal(userId));
     }
 
     @ApiOperation(value = "법인 처리 불가 항목 조회", notes = "법인 카드를 사용했으나, 처리가 불가한 항목을 확인합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 400, message = "잘못된 요청"),
+            @ApiResponse(code = 500, message = "서버 내부 오류")
+    })
     @GetMapping("/getRejected")
     public ResponseEntity<?> getRejected(@ApiIgnore @UserId Long userId) {
         return ResponseEntity.ok(receiptService.getRejected(userId));
     }
 
     @ApiOperation(value = "계좌 이체 후 완료 처리", notes = "법인 처리 불가 항목을 계좌이체 완료 하였을 시 상태를 deposit으로 변경합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 400, message = "잘못된 요청"),
+            @ApiResponse(code = 500, message = "서버 내부 오류")
+    })
     @PatchMapping("/completeDeposit")
     public ResponseEntity<?> changeState(@ApiIgnore @UserId Long userId,
                                          @RequestBody Long receiptProcessId) {
--- src/main/java/org/refit/spring/reward/controller/RewardController.java
+++ src/main/java/org/refit/spring/reward/controller/RewardController.java
@@ -2,6 +2,8 @@ package org.refit.spring.reward.controller;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.refit.spring.auth.annotation.UserId;
 import org.refit.spring.reward.dto.RewardListDto;
@@ -13,6 +15,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import springfox.documentation.annotations.ApiIgnore;
 
+import java.net.URI;
+
 @Api(tags = "리워드 API", description = "리워드 내역과 메인 화면용 포인트 리스트 관련 API입니다.")
 @RestController
 @RequestMapping("/api/reward")
@@ -22,6 +26,10 @@ public class RewardController {
     private final RewardService rewardService;
 
     @ApiOperation(value = "리워드 내역 조회", notes = "탄소중립포인트와 리워드 내역을 목록으로 조회합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 400, message = "잘못된 요청"),
+            @ApiResponse(code = 500, message = "서버 내부 오류")
+    })
     @GetMapping("")
     public ResponseEntity<?> getList(
             @ApiIgnore @UserId Long userId,
@@ -31,6 +39,10 @@ public class RewardController {
     }
 
     @ApiOperation(value = "포인트 리스트 조회", notes = "메인 페이지 포인트 리스트에서 보여지는 리워드들을 제공합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 400, message = "잘못된 요청"),
+            @ApiResponse(code = 500, message = "서버 내부 오류")
+    })
     @GetMapping("/getAllPoints")
     public ResponseEntity<?> getTotalPoint(
             @ApiIgnore @UserId Long userId) {
@@ -39,11 +51,16 @@ public class RewardController {
     }
 
     @ApiOperation(value = "지갑 상점 구매", notes = "상점에서 포인트로 지갑을 구매하면 포인트가 차감되고 보유 지갑 브랜드 테이블에 추가됩니다.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 400, message = "잘못된 요청"),
+            @ApiResponse(code = 500, message = "서버 내부 오류")
+    })
     @PostMapping("/purchase")
     public ResponseEntity<?> getWallet(
             @ApiIgnore @UserId Long userId,
             @RequestBody RewardWalletRequestDto rewardWalletRequestDto) {
         RewardWalletResponseDto dto = rewardService.purchaseWallet(userId, rewardWalletRequestDto);
-        return ResponseEntity.ok(dto);
+        URI location = URI.create("/reward/" + dto.getWalletId());
+        return ResponseEntity.created(location).body(dto);
     }
 }
