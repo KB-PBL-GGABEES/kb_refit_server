ad06538fbc1b304a982fb7b757b42c2bf5675cd2
suminiee <chansu0114@naver.com>
2025-07-30 10:28:37 +0900

    [FEAT] POS 상품 조회 API 구현

--- src/main/java/org/refit/spring/mapper/MerchandiseMapper.java
+++ src/main/java/org/refit/spring/mapper/MerchandiseMapper.java
@@ -4,8 +4,13 @@ import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 import org.refit.spring.merchandise.entity.Merchandise;
 
+import java.util.List;
+
 @Mapper
 public interface MerchandiseMapper {
     @Select("SELECT * FROM merchandise WHERE merchandise_id = #{merchandiseId}")
     Merchandise findByMerchandiseId(Long merchandiseId);
+
+    @Select("SELECT * FROM merchandise WHERE company_id = #{companyId}")
+    List<Merchandise> findAllByCompanyId(Long companyId);
 }
