965308805bce4d66b7974b3a347de8e795ad2fae
Cho2unseo <escces00@gmail.com>
2025-07-30 14:16:59 +0900

    [✨ FEAT] 뱃지 여부 확인 후 조건 달성 시 뱃지 보유 테이블에 추가

--- src/main/java/org/refit/spring/mapper/BadgeMapper.java
+++ src/main/java/org/refit/spring/mapper/BadgeMapper.java
@@ -1,6 +1,8 @@
 package org.refit.spring.mapper;
 
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 import org.refit.spring.wallet.entity.Badge;
 
@@ -13,4 +15,8 @@ public interface BadgeMapper {
 
     @Select("SELECT * FROM badge WHERE badge_id = #{badgeId}")
     Badge findById(Long badgeId);
+
+    @Insert("INSERT INTO personal_badge (is_worn, created_at, updated_at, badge_id, user_id) VALUES (0, now(), now(), #{badgeId}, #{userId})")
+    void insertPersonalBadge(@Param("badgeId") Long badgeId,
+                             @Param("userId") Long userId);
 }
