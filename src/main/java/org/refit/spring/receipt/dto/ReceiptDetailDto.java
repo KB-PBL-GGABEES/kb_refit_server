7000cf67f4543420a48a3d55dd68b565da7945b1
Cho2unseo <escces00@gmail.com>
2025-08-01 10:35:10 +0900

    [🚀 CHORE] 응답 수정

--- src/main/java/org/refit/spring/receipt/dto/ReceiptDetailDto.java
+++ src/main/java/org/refit/spring/receipt/dto/ReceiptDetailDto.java
@@ -1,6 +1,7 @@
 package org.refit.spring.receipt.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -9,6 +10,7 @@ import java.util.List;
 
 @Data
 @AllArgsConstructor
+
 public class ReceiptDetailDto {
     private Long userId;
     private Long receiptId;
@@ -22,7 +24,9 @@ public class ReceiptDetailDto {
     private String transactionType;
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
     private Date createdAt;
+    @ApiModelProperty(value = "영수 처리 여부", example = "accepted")
     private String processState;
     private String cardNumber;
+    @ApiModelProperty(value = "법인 카드 여부", example = "1")
     private Integer isCorporate;
 }
--- src/main/java/org/refit/spring/reward/dto/RewardWalletResponseDto.java
+++ src/main/java/org/refit/spring/reward/dto/RewardWalletResponseDto.java
@@ -1,11 +1,16 @@
 package org.refit.spring.reward.dto;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 @Data
 public class RewardWalletResponseDto {
+    @ApiModelProperty(value = "유저 아이디", example = "1")
     private Long userId;
+    @ApiModelProperty(value = "지갑 아이디", example = "2")
     private Long walletId;
+    @ApiModelProperty(value = "지갑 가격", example = "1400")
     private Long walletCost;
+    @ApiModelProperty(value = "남은 스타 포인트", example = "5000")
     private Long totalStarPoint;
 }
