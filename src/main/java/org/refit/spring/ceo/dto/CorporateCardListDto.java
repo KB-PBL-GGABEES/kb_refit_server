d548138f47a2e6ec651ac0f11e8f4e311cd00f3b
CHOKH <ghks027@gmail.com>
2025-08-01 10:24:16 +0900

    [♻️ REFACTOR] 페이지네이션 수정, swagger 적용

--- src/main/java/org/refit/spring/ceo/dto/CorporateCardListDto.java
+++ src/main/java/org/refit/spring/ceo/dto/CorporateCardListDto.java
@@ -1,6 +1,5 @@
 package org.refit.spring.ceo.dto;
 
-import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
