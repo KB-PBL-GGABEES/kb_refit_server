97a0e6b2d29e3e9fee75e72a3318154673f7ebae
Cho2unseo <escces00@gmail.com>
2025-08-05 17:11:38 +0900

    [🚀 CHORE] 불필요한 코드 삭제

--- src/main/java/org/refit/spring/reward/dto/RewardListDto.java
+++ src/main/java/org/refit/spring/reward/dto/RewardListDto.java
@@ -1,6 +1,5 @@
 package org.refit.spring.reward.dto;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.refit.spring.reward.entity.Reward;
