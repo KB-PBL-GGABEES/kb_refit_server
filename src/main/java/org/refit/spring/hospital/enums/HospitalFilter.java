1e6f8d970676254366c581919d3014aef6ac1a89
yeonhookim <rladusgn68@gmail.com>
2025-08-06 17:45:01 +0900

    [🐛FIX] 병원 영수증 필터 enum 수정

--- src/main/java/org/refit/spring/hospital/enums/HospitalFilter.java
+++ src/main/java/org/refit/spring/hospital/enums/HospitalFilter.java
@@ -4,19 +4,12 @@ import lombok.Getter;
 
 @Getter
 public enum HospitalFilter {
-    ALL(null),            // 모든 상태
-    ACCEPTED("accepted"), // 처리 완료
-    REJECTED("rejected"), // 반려
-    IN_PROGRESS("inProgress"), // 진행 중
-    NONE("none");         // 미처리 or null
+    ALL,
+    PROCESSED,
+    UNPROCESSED;
 
-    private final String dbValue;
-
-    HospitalFilter(String dbValue) {
-        this.dbValue = dbValue;
-    }
-
-    public boolean isAll() {
-        return this == ALL;
+    public boolean isProcessed() {
+        return this == PROCESSED;
     }
 }
+
