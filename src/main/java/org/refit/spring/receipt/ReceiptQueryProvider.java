ac0cc36a80b2c1f716ab2b8acbafe75026e86d1d
Cho2unseo <escces00@gmail.com>
2025-08-07 00:12:42 +0900

    [🐛 FIX] 구매영수증에서 병원영수증 제외

--- src/main/java/org/refit/spring/receipt/ReceiptQueryProvider.java
+++ src/main/java/org/refit/spring/receipt/ReceiptQueryProvider.java
@@ -9,7 +9,7 @@ import java.util.Map;
 public class ReceiptQueryProvider {
     public static String buildFilteredQuery(Map<String, Object> params) {
         StringBuilder sql = new StringBuilder("SELECT r.*, rp.process_state, c.company_name FROM receipt r LEFT OUTER JOIN receipt_process rp ON rp.receipt_id = r.receipt_id LEFT OUTER JOIN company c ON r.company_id = c.company_id WHERE r.user_id = #{userId}");
-
+        sql.append(" AND c.category_id != 1");
         ReceiptSort sort = (ReceiptSort) params.get("sort");
         if (sort == null) {
             sort = ReceiptSort.LATEST;
