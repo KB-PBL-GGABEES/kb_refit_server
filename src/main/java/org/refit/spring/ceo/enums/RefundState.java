018acb9d6c91269a96fcfd414bcf68875da4addc
CHOKH <ghks027@gmail.com>
2025-08-08 16:44:09 +0900

    [♻️ REFACTOR] 법인카드 필터 수정

--- src/main/java/org/refit/spring/ceo/enums/RefundState.java
+++ src/main/java/org/refit/spring/ceo/enums/RefundState.java
@@ -3,9 +3,7 @@ package org.refit.spring.ceo.enums;
 public enum RefundState {
     WHOLE, UNREFUND, REFUND;
 
-    public boolean UnRefund() {
-        return this == UNREFUND;
-    }
+    public boolean UnRefund() { return this == UNREFUND; }
 
     public boolean Refund() { return this == REFUND; }
 }
