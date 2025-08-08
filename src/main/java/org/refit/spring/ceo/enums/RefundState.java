f45d85a38401afd554f1c29a03b7b58ff3c6fc32
CHOKH <ghks027@gmail.com>
2025-08-08 14:48:36 +0900

    [♻️ REFACTOR] 법인카드 환불영수증 필터 오타 수정

--- src/main/java/org/refit/spring/ceo/enums/RefundState.java
+++ src/main/java/org/refit/spring/ceo/enums/RefundState.java
@@ -1,11 +1,11 @@
 package org.refit.spring.ceo.enums;
 
 public enum RefundState {
-    WHOLE, UNREFUND, FETUND;
+    WHOLE, UNREFUND, REFUND;
 
     public boolean UnRefund() {
         return this == UNREFUND;
     }
 
-    public boolean Refund() { return this == FETUND; }
+    public boolean Refund() { return this == REFUND; }
 }
