e4039573e4ea96edc49c82f8c6f14a232433c06c
CHOKH <ghks027@gmail.com>
2025-08-08 10:31:59 +0900

    [♻️ REFACTOR] 법인카드 정렬 카테고리 수정

--- src/main/java/org/refit/spring/ceo/enums/State.java
+++ src/main/java/org/refit/spring/ceo/enums/State.java
@@ -1,13 +1,19 @@
 package org.refit.spring.ceo.enums;
 
 public enum State {
-    Whole, Process, UnProcess;
+    Whole, Process, UnProcess, Accepted, Rejected, Deposit, InProgress;
 
     public boolean Process() {
         return this == Process;
     }
 
-    public boolean UnProcess() {
-        return this == UnProcess;
-    }
+    public boolean UnProcess() { return this == UnProcess; }
+
+    public boolean Rejected() { return this == Rejected; }
+
+    public boolean Accepted() { return this == Accepted; }
+
+    public boolean Deposit() { return this == Deposit; }
+
+    public boolean InProgress() { return this == InProgress; }
 }
