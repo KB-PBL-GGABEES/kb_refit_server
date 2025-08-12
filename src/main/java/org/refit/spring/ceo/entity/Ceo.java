90dadf44d48dca33e1826d56bbaf2200fcdeb34a
Cho2unseo <escces00@gmail.com>
2025-08-12 23:30:15 +0900

    [🐛 FIX] 리스트 날짜 형식 변경

--- src/main/java/org/refit/spring/ceo/entity/Ceo.java
+++ src/main/java/org/refit/spring/ceo/entity/Ceo.java
@@ -2,7 +2,7 @@ package org.refit.spring.ceo.entity;
 
 import lombok.*;
 
-import java.time.LocalDateTime;
+import java.util.Date;
 
 @Data
 @Builder
@@ -14,6 +14,6 @@ public class Ceo {
     private Long receiptId;
     private String companyName; // 상호명
     private Long totalPrice;    // 결제금액
-    private LocalDateTime createdAt; // 결제 일시
+    private Date createdAt; // 결제 일시
     private String processState; // 영수처리 여부
 }
